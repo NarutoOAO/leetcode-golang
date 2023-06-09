841251edc9918f361feec1af2fde7024a30a1c2f
Yihang Yu <2371848072@qq.com>
2023-06-09 13:33:02 +1000

    upup

--- leetcode/0199/0199.go
+++ leetcode/0199/0199.go
@@ -1,13 +1,11 @@
 package _199
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func rightSideView(root *TreeNode) []int {
 	ans := []int{}
 	if root == nil {
