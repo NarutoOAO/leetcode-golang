ccfedc8ae3d31c8b909b26ce200ebcb6ccaadaf8
Yihang Yu <2371848072@qq.com>
2023-06-09 13:31:46 +1000

    upup

--- leetcode/0543/0543.go
+++ leetcode/0543/0543.go
@@ -1,13 +1,11 @@
 package _543
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 func diameterOfBinaryTree(root *TreeNode) int {
 	if root == nil {
 		return 0
