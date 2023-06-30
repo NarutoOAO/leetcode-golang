9c08611a68468149e865135da3d13322981d33c1
Yihang Yu <2371848072@qq.com>
2023-06-30 23:17:27 +1000

    upup

---
+++ 剑指offer/058/058-1.go
@@ -0,0 +1,15 @@
+package _58
+
+import "strings"
+
+func reverseWords(s string) string {
+	s = strings.Trim(s, " ")
+	words := strings.Fields(s)
+	i, j := 0, len(words)-1
+	for i < j {
+		words[i], words[j] = words[j], words[i]
+		i++
+		j--
+	}
+	return strings.Join(words, " ")
+}
