88b1d47fcba6b185ed9164ee363b7b8a7049deef
Yihang Yu <2371848072@qq.com>
2023-07-03 10:53:17 +1000

    upup

---
+++ leetcode/周赛2/6894.go
@@ -0,0 +1,19 @@
+package 周赛2
+
+func sumImbalanceNumbers(nums []int) int {
+	res, cnt, n := 0, 0, len(nums)
+	for i, v1 := range nums {
+		vis := make(map[int]int)
+		vis[v1] = 1
+		cnt = 0
+		for j := i + 1; j < n; j++ {
+			v2 := nums[j]
+			if vis[v2] == 0 {
+				cnt += 1 - vis[v2-1] - vis[v2+1]
+				vis[v2] = 1
+			}
+			res += cnt
+		}
+	}
+	return res
+}
