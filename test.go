e60f0e581662166770e2b22c9dc59e420087b654
Yihang Yu <2371848072@qq.com>
2023-07-10 22:55:13 +1000

    upup

--- test.go
+++ test.go
@@ -3,49 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	root := &TreeNode{
-		Val:   1,
-		Left:  nil,
-		Right: nil,
-	}
-	fmt.Println(pathSum(root, 1))
+	fmt.Println(maxNonDecreasingLength([]int{11, 7, 7, 9}, []int{19, 19, 1, 7}))
 }
 
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-var res [][]int
-
-func pathSum(root *TreeNode, target int) [][]int {
-	if root == nil {
-		return [][]int{}
+func maxNonDecreasingLength(nums1 []int, nums2 []int) int {
+	preMax, sum, n, res := 0, 0, len(nums1), 0
+	for i := 0; i < n; i++ {
+		if min(nums1[i], nums2[i]) >= preMax {
+			preMax = min(nums1[i], nums2[i])
+			sum++
+		} else if max(nums1[i], nums2[i]) >= preMax {
+			preMax = max(nums1[i], nums2[i])
+			sum++
+		} else {
+			sum = 1
+			preMax = min(nums1[i], nums2[i])
+		}
+		res = max(res, sum)
 	}
-	recur(root, target, []int{})
 	return res
-
 }
 
-func recur(root *TreeNode, target int, tmp []int) {
-	target -= root.Val
-	if target < 0 {
-		return
+func min(x, y int) int {
+	if x < y {
+		return x
 	}
-	tmp = append(tmp, root.Val)
-	if root.Left == nil && root.Right == nil && target == 0 {
-		tmp1 := make([]int, len(tmp))
-		copy(tmp1, tmp)
-		res = append(res, tmp1)
-		return
-	}
-	if root.Left != nil {
-		recur(root.Left, target, tmp)
-	}
-	if root.Right != nil {
-		recur(root.Right, target, tmp)
-	}
+	return y
+}
 
-	//tmp = tmp[:len(tmp)-1]
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
 }
